2a735f4c030ac85ae8f38e75c7edb9322e96ddbb
Jhon Sebastian Bonilla Quicazan <sebastianbonilla423@gmail.com>
2026-03-17 12:41:07 -0500

    Add files via upload

---
+++ fundamentos_de_logica_de_programacion/portafolio.py/ejercicio.py
@@ -0,0 +1,14 @@
+##tortas de vainilla con rellenas
+# horno temperatura 350ºf
+#pero el horno muestra al temperatura en ºc 
+# formula ºc=(ºf-32)/1.8
+
+
+
+f_input=input("ingrese la temperatura en ºf:")
+f=float(f_input)
+c=(f-32)/1.8
+print("la temperatura en ºc es:",c)
+
+
+
