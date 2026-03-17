452d934605dbeb7e7d727b8fdc5022af86b3dad3
Jhon Sebastian Bonilla Quicazan <sebastianbonilla423@gmail.com>
2026-03-17 12:45:10 -0500

    Refactor comments and maintain temperature conversion

--- fundamentos_de_logica_de_programacion/portafolio.py/ejercicio.py
+++ fundamentos_de_logica_de_programacion/portafolio.py/ejercicio.py
@@ -1,8 +1,8 @@
-##tortas de vainilla con relleno
-# horno temperatura 350ºf
-#pero el horno muestra al temperatura en ºc 
-# formula ºc=(ºf-32)/1.8
-
+#tortas de vainilla con relleno
+#la temperatura del horno es 350ºf
+#pero el horno muestra al temperatura en ºf
+#formula ºc=(ºf-32)/1.8
+#necesitamos ver la temerrautra en ºc
 
 
 f_input=input("ingrese la temperatura en ºf:")
