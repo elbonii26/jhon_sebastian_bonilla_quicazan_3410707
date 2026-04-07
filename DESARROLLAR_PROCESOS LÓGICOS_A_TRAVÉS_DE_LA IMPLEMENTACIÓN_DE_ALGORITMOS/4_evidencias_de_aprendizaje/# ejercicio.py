d66190f10ef059af2b6acd75a51841fa5c911019
Jhon Sebastian Bonilla Quicazan <sebastianbonilla423@gmail.com>
2026-04-06 20:43:37 -0500

    Delete DESARROLLAR_PROCESOS LÓGICOS_A_TRAVÉS_DE_LA IMPLEMENTACIÓN_DE_ALGORITMOS/4_evidencias_de_aprendizaje/# ejercicio.py

--- DESARROLLAR_PROCESOS LÓGICOS_A_TRAVÉS_DE_LA IMPLEMENTACIÓN_DE_ALGORITMOS/4_evidencias_de_aprendizaje/# ejercicio.py
+++
@@ -1,9 +0,0 @@
-# ejercicio.py
-# Este es un archivo de ejemplo para un ejercicio en Python.
-
-def main():
-    print("¡Hola, mundo! Este es un ejercicio básico en Python.")
-
-if __name__ == "__main__":
-    main()
-    
