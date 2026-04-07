03effcec2b6269bdc16c8d2253767a71c1b55dc6
Jhon Sebastian Bonilla Quicazan <sebastianbonilla423@gmail.com>
2026-04-06 21:11:23 -0500

    Add files via upload

---
+++ DESARROLLAR_PROCESOS LÓGICOS_A_TRAVÉS_DE_LA IMPLEMENTACIÓN_DE_ALGORITMOS/4_evidencias_de_aprendizaje/login.py
@@ -0,0 +1,16 @@
+# usuario tiene que ingresar el ususario y la contraseña
+usuario = input ("Ingrese su usuario: ")
+contraseña = input("ingrese su contraseña: ")
+
+
+
+# validar si el usuario es correcto y la contraseña es correcta
+if usuario == "sebastian" and contraseña == "123456":
+    print("!!bienvenido¡¡")
+    
+
+
+# si el usuario es incorrecto o la contraseña es incorrecta, mostrar un mensaje de error
+else: print("el usuario o la contraseña son incorrectos")
+
+
